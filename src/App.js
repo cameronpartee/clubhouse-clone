57ef1b02ad81fd7e80dbe0026d1e83eeda30c816
Cameron Partee <cp3082@nyu.edu>
2021-11-03 22:43:13 -0400

    update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Welcome from "./pages/Welcome";
 import PlannedLayout from "./pages/Layout/PlannedLayout";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import PhoneConfirmation from "./pages/PhoneConfirmation";
+import CodeConfirm from "./pages/CodeConfirm";
 
 function App() {
   return (
     <BrowserRouter>
       <PlannedLayout>
-        <Welcome />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/invite" component={PhoneConfirmation} />
+        <Route exact path="/code_confirm" component={CodeConfirm} />
       </PlannedLayout>
     </BrowserRouter>
   );
