ae704c736cb9c2e0bf4434539453ccfb5ce8c656
Cameron Partee <cp3082@nyu.edu>
2021-11-04 22:49:32 -0400

    update app styling and add more routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import "./App.css";
 import Welcome from "./pages/Welcome";
 import PlannedLayout from "./pages/Layout/PlannedLayout";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PhoneConfirmation from "./pages/PhoneConfirmation";
 import CodeConfirm from "./pages/CodeConfirm";
+import AllowNotification from "./pages/AllowNotification";
+import AppLayout from "./pages/Layout/AppLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <PlannedLayout>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/invite" component={PhoneConfirmation} />
-        <Route exact path="/code_confirm" component={CodeConfirm} />
-      </PlannedLayout>
+      <Route
+        exact
+        path={[
+          "/",
+          "/invite",
+          "/get_username",
+          "/code_confirm",
+          "/allow_notification",
+        ]}
+      >
+        <PlannedLayout>
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/invite" component={PhoneConfirmation} />
+            <Route exact path="/code_confirm" component={CodeConfirm} />
+            <Route
+              exact
+              path="/allow_notification"
+              component={AllowNotification}
+            />
+          </Switch>
+        </PlannedLayout>
+      </Route>
+      <Route exact path={["/home"]}>
+        <AppLayout>
+          <Switch>
+            <Route exact path="/home" component={() => <Home />} />
+          </Switch>
+        </AppLayout>
+      </Route>
     </BrowserRouter>
   );
 }
