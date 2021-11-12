57a98452c7b8f6b8fcb6ce0720c820d88dce67e0
cameronpartee <cameronepartee@gmail.com>
2021-11-12 14:13:34 -0500

    add json and bottom sheet

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -1,6 +1,7 @@
 import SwipeableBottomSheet from "react-swipeable-bottom-sheet";
 import style from "../style/bottomSheet.module.css";
 import StartRoom from "./bottom_sheets/StartRoom";
+import NewRoom from "./bottom_sheets/NewRoom";
 
 export default function BottomSheet(props) {
   return (
@@ -15,16 +16,28 @@ export default function BottomSheet(props) {
       <div
         className={style.BottomSheetContainer}
         style={{
-          backgroundColor: props.sheetTitle == "profile" ? "something" : "else",
+          backgroundColor: props.sheetTitle == "profile" ? "transparent" : "",
         }}
       >
-        <StartRoom
-          setSheetCreateRoom={props.setSheetCreateRoom}
-          setSheetVisible={(item) => {
-            props.setSheetVisible(item);
-            props.setItemsVisible(true);
-          }}
-        />
+        {props.sheetTitle == "new room" ? (
+          <NewRoom
+            cardDetail={props.cardDetail}
+            setSheetVisible={(item) => {
+              props.setSheetVisible(item);
+              props.setItemsVisible(true);
+            }}
+          />
+        ) : props.sheetTitle == "start room" ? (
+          <StartRoom
+            setSheetCreateRoom={props.setSheetCreateRoom}
+            setSheetVisible={(item) => {
+              props.setSheetVisible(item);
+              props.setItemsVisible(true);
+            }}
+          />
+        ) : (
+          ""
+        )}
       </div>
     </SwipeableBottomSheet>
   );
